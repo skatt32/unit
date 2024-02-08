57baad969d6e0f63c89abecc68ac038a454082e7
Neto test <test@neto.com>
2024-02-08 16:50:31 +0400

    New branch

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,18 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldCorrectAmount() {
+        assertEquals(service.remain(900), 100);
+
+    }
+
+
+}
